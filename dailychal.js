6bd3646492e54c32aea8fd1efe4fc29ec4ed6520
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-05-23 17:57:48 +0300

    added

--- dailychal.js
+++ dailychal.js
@@ -30,3 +30,14 @@ function getMiddle(str){
     return str[Math.floor(str.length / 2)]
   }
 }
+
+function dnaStrand(dna){
+ return dna.split('').map((l) =>{
+    if(l==='A')  return 'T' 
+     if(l==='T') return 'A'
+     if(l==='C') return 'G'
+     if(l==='G') return 'C'
+  } ).join('')
+  
+  
+}
