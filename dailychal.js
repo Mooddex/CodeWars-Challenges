492a608ffcd9315d62522b200e9fdea00d961b27
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-05-25 07:08:39 +0300

    another one

--- dailychal.js
+++ dailychal.js
@@ -52,3 +52,17 @@ function frame(balls) {
   if (score > 147) return 'invalid data'
   return score
 }
+
+//https://www.codewars.com/kata/514b92a657cdc65150000006/solutions/javascript
+function solution(number) {
+  if (number <= 0) return 0;
+
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
