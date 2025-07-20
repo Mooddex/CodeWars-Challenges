60b73cb0d2ce6cf4b3eec07215e7100bfd3cca68
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-07-20 22:29:54 +0300

    added twoSum function to find indices of two numbers that add up to a target

--- leetCode.js
+++ leetCode.js
@@ -1,4 +1,15 @@
 
+
+//------------------
+var twoSum = function(nums, target) {
+    for(i=0; i<nums.length ; i++ ){
+        for(z=i+1; z<nums.length ; z++){
+            if (nums[i]+nums[z]=== target){
+                return [i,z]
+            }
+        }
+    }
+};
 //-----------------------
 var containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
