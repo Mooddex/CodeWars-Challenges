495c19f43a90ca57aca1a15312ef4c581cd9e287
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-07-30 20:52:51 +0300

    removeDuplicates function to return the count of unique elements in an array

--- leetCode.js
+++ leetCode.js
@@ -1,5 +1,19 @@
 
+//--------------
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
 
+    let i = 0;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;              // نتحرك لمكان جديد
+            nums[i] = nums[j]; // نحط فيه العنصر الجديد
+        }
+    }
+
+    return i + 1; // عدد العناصر الفريدة
+};
 //------------------
 var twoSum = function(nums, target) {
     for(i=0; i<nums.length ; i++ ){
